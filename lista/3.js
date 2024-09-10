5ad9d0abe5be1f8f721f50337227dbf22b90bb08
Guilherme <111362221+Guilhrme-hen@users.noreply.github.com>
2024-09-10 13:45:10 -0300

    Create 3.js

---
+++ lista/3.js
@@ -0,0 +1,10 @@
+var numero = prompt("Digite um número:");
+
+numero = parseInt(numero);
+
+if (numero % 2 === 0) {
+    alert("O número " + numero + " é par.");
+} else {
+    alert("O número " + numero + " é ímpar.");
+}
+
