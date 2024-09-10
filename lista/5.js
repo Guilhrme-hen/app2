d99612ef627a10b4944f90555fd9a803ba5e3b59
Guilherme <111362221+Guilhrme-hen@users.noreply.github.com>
2024-09-10 13:47:17 -0300

    Create 5.js

---
+++ lista/5.js
@@ -0,0 +1,6 @@
+var palavra = prompt("Digite uma palavra:");
+
+var palavraMaiuscula = palavra.toUpperCase();
+var palavraMinuscula = palavra.toLowerCase();
+
+alert("Palavra em maiúsculas: " + palavraMaiuscula + "\nPalavra em minúsculas: " + palavraMinuscula);
