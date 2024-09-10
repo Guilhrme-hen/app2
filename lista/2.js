08aad21dbf8abdba74c025d61814f1eea26bda48
Guilherme <111362221+Guilhrme-hen@users.noreply.github.com>
2024-09-10 13:43:41 -0300

    Create 2.js

---
+++ lista/2.js
@@ -0,0 +1,12 @@
+var numero1 = prompt("Digite o primeiro número:");
+var numero2 = prompt("Digite o segundo número:");
+
+numero1 = parseFloat(numero1);
+numero2 = parseFloat(numero2);
+
+var soma = numero1 + numero2;
+var subtracao = numero1 - numero2;
+var multiplicacao = numero1 * numero2;
+var divisao = numero1 / numero2;
+
+alert("Soma: " + soma + "\nSubtração: " + subtracao + "\nMultiplicação: " + multiplicacao + "\nDivisão: " + divisao);
